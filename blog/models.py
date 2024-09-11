fd5301b6ee5ea314e8a8c4aee69cd9b1ec6838e8
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-11 13:34:58 -0400

    migrated blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
+# blog/models.py
+
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    "blog post"
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now = True)
