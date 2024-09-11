3cce1355e78fb53c7ffbb438df66aaa0ce9b0954
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-11 14:38:22 -0400

    Blog App Updated
    added many fields, admin settings, and migration

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
-from django.contrib import admin
+# blog/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from . import models
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'created', 'updated',)
+    search_fields = ('title', 'author__username',)
+    list_filter = ('status', 'created', 'updated', 'author',)
+    prepopulated_fields = {'slug': ('title',)}
+
+admin.site.register(models.Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,55 @@
 # blog/models.py
 
 from django.db import models
+from django.conf import settings
 
 class Post(models.Model):
-    "blog post"
+    """blog post"""
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published')
+    ]
+
+    # fields
     title = models.CharField(max_length=255)
+
+    slug = models.SlugField(
+        null=False,
+        help_text='The date & time this article was published',
+        unique_for_date='published',
+    )
+
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.PROTECT,
+        related_name='blog_posts',
+        null=False
+    )
+
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+        help_text='Set to "published" to make this post publicly visible',
+    )
+
     content = models.TextField()
+
+    published = models.DateTimeField(
+        null=True,
+        blank=True,
+        help_text='The date & time this article was published',
+    )
+
     created = models.DateTimeField(auto_now_add=True)
+
     updated = models.DateTimeField(auto_now = True)
+
+
+    class Meta:
+        ordering = ['-created',]
+
+    def __str__(self):
+        return str(self.title)
