87055734660a203e3270f015ae00985c25c298a6
Morgan <109176008+morgs999@users.noreply.github.com>
2024-09-12 16:46:59 -0400

    added topics to blogs
    via the ipython shell

--- blog/admin.py
+++ blog/admin.py
@@ -6,7 +6,12 @@ from . import models
 class PostAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'created', 'updated',)
     search_fields = ('title', 'author__username',)
-    list_filter = ('status', 'created', 'updated', 'author',)
+    list_filter = ('status', 'created', 'updated', 'author', 'topics',)
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(models.Post, PostAdmin)
+
+@admin.register(models.Topic)
+class TopicAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+    prepopulated_fields = {'slug': ('name',)}
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,21 @@
 from django.db import models
 from django.conf import settings
 
+class Topic(models.Model):
+    """blog post topic"""
+    name = models.CharField(
+        max_length=50,
+        unique=True
+    )
+    slug=models.SlugField(unique=True)
+
+    def __str__(self):
+        return str(self.name)
+    
+    class Meta:
+        ordering = ['name']
+
+
 class Post(models.Model):
     """blog post"""
     DRAFT = 'draft'
@@ -47,9 +62,15 @@ class Post(models.Model):
 
     updated = models.DateTimeField(auto_now = True)
 
+    topics = models.ManyToManyField(
+        Topic,
+        related_name='blog_posts'
+    )
+
 
     class Meta:
         ordering = ['-created',]
 
     def __str__(self):
         return str(self.title)
+
