a57c1847681988044cd00770a39bc9a2b9c2b281
Morgan <109176008+morgs999@users.noreply.github.com>
2024-11-07 11:37:53 -0500

    quick css updates

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -2,7 +2,7 @@
 Django settings for myBlog project.
 """
 
-# import os
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,8 +115,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    # os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static')
 ]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
