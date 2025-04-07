5fb0cfec8f40d2877be0563f24b0289955d0fedd
Morgan <109176008+morgs999@users.noreply.github.com>
2025-04-07 11:48:00 -0400

    update github workflow

--- myBlog/settings.py
+++ myBlog/settings.py
@@ -86,16 +86,6 @@ WSGI_APPLICATION = 'myBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'postgres',
-#         'USER': 'clarkmor',
-#         'PASSWORD': 'sheridan@db99',
-#         'HOST': 'morganliteratureblog.postgres.database.azure.com',
-#         'PORT': '5432',
-#     }
-# }
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
